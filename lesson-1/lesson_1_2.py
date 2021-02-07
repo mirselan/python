68166f175c8353ae4af506778ae2a8f46391951d
mirselan <78569865+mirselan@users.noreply.github.com>
2021-02-07 20:56:56 +0300

    Delete lesson-1 directory

--- lesson-1/lesson_1_2.py
+++
@@ -1,7 +0,0 @@
-time = int(input("Введите время в секундах: "))
-sec = time % 60
-minute = time // 60
-minute_1 = minute % 60
-hour = minute // 60
-
-print(f"Ваше время: {hour:02d}: {minute_1:02d}: {sec:02d}")
