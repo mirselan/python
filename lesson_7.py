2899ca79c4c22e3e806ecbae060f8a00dfa94ba4
mirselan <sergei.lanikhin@gmail.com>
2021-03-13 18:51:17 +0300

    ПЗ_7

--- lesson_7.py
+++ lesson_7.py
@@ -64,4 +64,69 @@ suit = Suit(180)
 print(coat + suit)
 
 """
-# Задание_3 сделать не успел до дедлайна
+"""
+# Задание_3 
+
+
+def cheek(fun):
+    def cheek_obj(*args, **kwargs):
+        if not isinstance(args[1], type(args[0])):
+            raise TypeError(f'В функцию передан объект не типа {type(args[0])}')
+        return fun(*args, **kwargs)
+    return cheek_obj
+
+
+class OrganicCell:
+    volume = 5  # Ячеек в ряду
+
+    def __init__(self, number: int):
+        self.number = number
+
+    def __str__(self) -> str:
+        i = self.number // self.volume
+        j = self.number % self.volume
+        lists = [*['*' * self.volume for _ in range(i)], '*' * j]
+        return '\n'.join(lists)
+
+    @cheek
+    def __add__(self, obj: object) -> object:
+        return OrganicCell(self.number + obj.number)
+
+    @cheek
+    def __sub__(self, obj) -> object:
+        if self.number < obj.number:
+            raise AssertionError("Разность количества ячеек двух клеток меньше нуля")
+        return OrganicCell(self.number - obj.number)
+
+    @cheek
+    def __mul__(self, obj) -> object:
+        return OrganicCell(self.number * obj.number)
+
+    @cheek
+    def __truediv__(self, obj) -> object:
+        if not obj.number:
+            raise ZeroDivisionError("Клетка-делитель пуста")
+        return OrganicCell(self.number // obj.number)
+
+    def make_order(self, volume: int) -> str:
+        self.volume = volume
+        print(self.__str__())
+
+
+a = OrganicCell(12)
+b = OrganicCell(5)
+try:
+    print(f"Сумма:\n{a + b}")
+    print(f"Разность:\n{a - b}")
+    print(f"Умножение:\n{a * b}")
+    print(f"Деление:\n{a / b}")
+    print(f"Замена количества ячеек в ряду:")
+    a.make_order(10)
+    print(f"Замена количества ячеек в ряду:")
+    b.make_order(10)
+    print(f"Замена количества ячеек в ряду:")
+    (a * b).make_order(10)
+except Exception as err:
+    print(f'Ошибка: {err}')
+
+"""
